a18fb04d9a3f22eb53ac8adc7ad3713f0bc74b44
Abdulrazaq Alhasan <alhsn.me@gmail.com>
2022-06-22 09:20:57 +0200

    Update

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-const hamburgar = document.querySelector('.fa-bars')
-const navUl = document.querySelector('nav ul')
+// Toggle hamburgar
+const hamburgar = document.querySelector(".fa-bars");
+const navUl = document.querySelector("nav ul");
+const navLinks = document.querySelectorAll("nav ul li");
 
+hamburgar.addEventListener("click", () => {
+  navUl.classList.toggle("active");
+});
 
-hamburgar.addEventListener('click', () => {
-    navUl.classList.toggle('show')
-})
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    navUl.classList.toggle("active");
+  });
+});
+
+// Dark mood
+const moon = document.getElementById("moon");
+const cvBtn = document.getElementById("cv-btn");
+
+moon.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    moon.src = "img/sun.png";
+  } else {
+    moon.src = "img/moon.png";
+  }
+});
+
+cvBtn.addEventListener("click", function () {
+  window.open(
+    "https://www.mediafire.com/file/i3a401nrjv9925m/CV_1.docx/file",
+    "blank"
+  );
+});
